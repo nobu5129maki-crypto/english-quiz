5748ad363822d196ca24e1d19f150a2fa667cff6
nobu5129maki-crypto <nobu5129maki@gmail.com>
2026-05-16 14:01:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'word-master-v2';
+const CACHE_NAME = 'word-master-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -8,8 +8,17 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
